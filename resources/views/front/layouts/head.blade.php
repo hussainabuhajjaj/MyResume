141ba9f19c4a3751d03272976cfe842a224e2cc4
hussainabuhajjaj <hussain.h.ff32@gmail.com>
2023-02-05 22:12:49 +0400

    second commit

---
+++ resources/views/front/layouts/head.blade.php
@@ -0,0 +1,21 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <title>BreezyCV - Resume / CV / vCard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="BreezyCV - Resume / CV / vCard Template" />
+    <meta name="keywords" content="vcard, resposnive, retina, resume, jquery, css3, bootstrap, Material CV, portfolio" />
+    <meta name="author" content="lmpixels" />
+    <link rel="shortcut icon" href="favicon.ico">
+
+
+    <link rel="stylesheet" href="{{asset('frontassets/css/reset.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('frontassets/css/bootstrap-grid.min.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('frontassets/css/animations.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('frontassets/css/perfect-scrollbar.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('frontassets/css/owl.carousel.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('frontassets/css/magnific-popup.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('frontassets/css/main.css')}}" type="text/css">
+
+    <script src="js/modernizr.custom.js"></script>
+  </head>
---
+++ resources/views/front/layouts/js.blade.php
@@ -0,0 +1,15 @@
+
+    <script data-cfasync="false" src="../../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script src="{{asset('frontassets/js/jquery-2.1.3.min.js')}}"></script>
+    <script src="{{asset('frontassets/js/modernizr.custom.js')}}"></script>
+    <script src="{{asset('frontassets/js/animating.js')}}"></script>
+    <script src="{{asset('frontassets/js/imagesloaded.pkgd.min.js')}}"></script>
+    <script src='=/../../../../../www.google.com/recaptcha/api.js'></script>
+    <script src='{{asset('frontassets/js/perfect-scrollbar.min.js')}}'></script>
+    <script src='{{asset('frontassets/js/jquery.shuffle.min.js')}}'></script>
+    <script src='{{asset('frontassets/js/masonry.pkgd.min.js')}}'></script>
+    <script src='{{asset('frontassets/js/owl.carousel.min.js')}}'></script>
+    <script src="{{asset('frontassets/js/jquery.magnific-popup.min.js')}}"></script>
+    <script src="{{asset('frontassets/js/validator.js')}}"></script>
+    <script src="{{asset('frontassets/js/main.js')}}"></script>
+  <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993" integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA==" data-cf-beacon='{"rayId":"7941d39fbb000451","version":"2022.11.3","r":1,"token":"94b99c0576dc45bf9d669fb5e9256829","si":100}' crossorigin="anonymous"></script>
